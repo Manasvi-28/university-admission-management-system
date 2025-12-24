2db32b655fe4431942bfa4a5342990b3ab135a41
Manasvi Nautiyal <manasvimanas93@gmail.com>
2025-12-24 13:15:21 +0530

    Add gitignore

--- ap.py
+++
@@ -1,5 +0,0 @@
-from flask import send_from_directory,Flask
-app=Flask(__name__)
-@app.route("/favicon.ico")
-def favicon():
-    return send_from_directory("static", "favicon.ico", mimetype="image/vnd.microsoft.icon")
